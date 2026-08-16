6612b15edddde046e3f50191398693e3278727ff
spk-DA <spk011251@gmail.com>
2026-08-16 21:24:48 +0530

    Create 2_create_table.sql

---
+++ RetailStore_Sales_Analysis/2_create_table.sql
@@ -0,0 +1,19 @@
+Step 2: Create Table
+CREATE TABLE store_sales
+(
+sale_id INT PRIMARY KEY AUTO_INCREMENT,
+order_date DATE,
+customer_name VARCHAR(50),
+gender VARCHAR(10),
+age INT,
+city VARCHAR(40),
+state VARCHAR(40),
+product_name VARCHAR(60),
+category VARCHAR(40),
+quantity INT,
+unit_price DECIMAL(10,2),
+discount DECIMAL(10,2),
+total_amount DECIMAL(10,2),
+payment_mode VARCHAR(20),
+salesperson VARCHAR(50)
+);
